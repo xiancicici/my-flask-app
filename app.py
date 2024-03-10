77b2087da80a81c7f0a29c968fa45dcdc786aa21
Seth <61952590+xiancicici@users.noreply.github.com>
2024-03-10 18:55:01 +0000

    add requirements

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 
+page_name = "Home"
+
 @app.route('/')
-def get():
-  return "<p>Hello World</p>"
+def hello_world():
+  return render_template('home.html', page_name=page_name)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
